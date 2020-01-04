c81304c332ff3d1f27afff8cb55d78b016f20cc5
cuishangfei <491735397@qq.com>
2020-01-04 17:23:01 +0800

    项目

--- src/main/java/com/tcbd07/mintproject/controller/ProgramsController.java
+++ src/main/java/com/tcbd07/mintproject/controller/ProgramsController.java
@@ -5,6 +5,7 @@ import com.google.common.graph.Network;
 import com.tcbd07.mintproject.entity.ProgramsVo;
 import com.tcbd07.mintproject.entity.User;
 import com.tcbd07.mintproject.service.ProgramsVoService;
+import com.tcbd07.mintproject.util.ResultMessage;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.elasticsearch.annotations.FieldType;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -18,13 +19,24 @@ public class ProgramsController {
     private ProgramsVoService programsVoService;
 
     @GetMapping("/queryAllProgramsVo")
-    public List<ProgramsVo> get(){
-        return programsVoService.queryAllProgramsVo();
+    public ResultMessage queryAllProgramsVo(){
+        List<ProgramsVo> programsVoList = programsVoService.queryAllProgramsVo();
+        return ResultMessage.success(programsVoList);
     }
+
+    @GetMapping("/getProgramsVo")
+    public ResultMessage getProgramsVo(String programId){
+        ProgramsVo programsVo = programsVoService.getProgramsVo(programId);
+        return ResultMessage.success(programsVo);
+    }
+
     @GetMapping("/getAllFirms")
-    public List<User> get1(){
-        return programsVoService.getAllFirms("2");
+    public ResultMessage getAllFirms(String firmId){
+        List<User> userList = programsVoService.getAllFirms(firmId);
+        return ResultMessage.success(userList);
     }
+
+
 
 
 
