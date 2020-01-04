fb37e0aa1aec1e1ac3553f0aa96e1b6e3883975e
cuishangfei <491735397@qq.com>
2020-01-04 17:29:04 +0800

    项目

--- src/main/java/com/tcbd07/mintproject/controller/ProgramsController.java
+++ src/main/java/com/tcbd07/mintproject/controller/ProgramsController.java
@@ -6,6 +6,10 @@ import com.tcbd07.mintproject.entity.ProgramsVo;
 import com.tcbd07.mintproject.entity.User;
 import com.tcbd07.mintproject.service.ProgramsVoService;
 import com.tcbd07.mintproject.util.ResultMessage;
+import io.swagger.annotations.ApiImplicitParam;
+import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiResponse;
+import io.swagger.annotations.ApiResponses;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.elasticsearch.annotations.FieldType;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -18,18 +22,34 @@ public class ProgramsController {
     @Autowired
     private ProgramsVoService programsVoService;
 
+    @ApiOperation(value = "查询所有项目（含商家）",notes = "请仔细查看！")
+    @ApiResponses(value = {
+            @ApiResponse(code = 200,message = "查询成功"),
+            @ApiResponse(code = 403,message = "网络异常")
+    })
     @GetMapping("/queryAllProgramsVo")
     public ResultMessage queryAllProgramsVo(){
         List<ProgramsVo> programsVoList = programsVoService.queryAllProgramsVo();
         return ResultMessage.success(programsVoList);
     }
-
+    @ApiOperation(value = "根据项目id查询单个项目（含商家）",notes = "请仔细查看！")
+    @ApiImplicitParam(name = "programId",value = "programId",dataType = "String",example = "项目id")
+    @ApiResponses(value = {
+            @ApiResponse(code = 200,message = "查询成功"),
+            @ApiResponse(code = 403,message = "网络异常")
+    })
     @GetMapping("/getProgramsVo")
     public ResultMessage getProgramsVo(String programId){
         ProgramsVo programsVo = programsVoService.getProgramsVo(programId);
         return ResultMessage.success(programsVo);
     }
 
+    @ApiOperation(value = "根据商家id查询用户",notes = "请仔细查看！，多个商户")
+    @ApiImplicitParam(name = "firmId",value = "firmId",dataType = "String",example = "商家id")
+    @ApiResponses(value = {
+            @ApiResponse(code = 200,message = "查询成功"),
+            @ApiResponse(code = 403,message = "网络异常")
+    })
     @GetMapping("/getAllFirms")
     public ResultMessage getAllFirms(String firmId){
         List<User> userList = programsVoService.getAllFirms(firmId);
