b9281ad493ea898e7e5a83389f74a4a9cf0baa59
BonePoints <704661673@qq.com>
2020-01-04 11:17:16 +0800

    简单新增修改

--- src/main/java/com/tcbd07/mintproject/controller/NewsController.java
+++ src/main/java/com/tcbd07/mintproject/controller/NewsController.java
@@ -4,6 +4,8 @@ import com.alibaba.fastjson.JSON;
 import com.tcbd07.mintproject.dao.NewsESRepository;
 import com.tcbd07.mintproject.entity.News;
 import com.tcbd07.mintproject.service.NewsService;
+import com.tcbd07.mintproject.util.ActiveMQUtils;
+import com.tcbd07.mintproject.util.CommonEnum;
 import com.tcbd07.mintproject.util.ResultMessage;
 import io.swagger.annotations.ApiImplicitParam;
 import io.swagger.annotations.ApiOperation;
@@ -14,6 +16,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.elasticsearch.core.query.NativeSearchQueryBuilder;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -26,6 +29,8 @@ public class NewsController {
     private NewsService newsService;
     @Autowired
     private NewsESRepository newsESRepository;
+    @Autowired
+    private ActiveMQUtils activeMQUtils;
 
     @RequestMapping("/")
     public String home(){
@@ -56,6 +61,47 @@ public class NewsController {
         map.put("newsList",s);
         map.put("count",count);
         return ResultMessage.success(map);
+    }
+
+    @ApiOperation(value = "这是添加新闻功能",notes = "查询新闻")
+    @ApiImplicitParam(name = "news",value = "news",dataType = "News")
+    @ApiResponses(value = {
+            @ApiResponse(code = 200,message = "添加成功"),
+            @ApiResponse(code = 500,message = "用户不能为空"),
+            @ApiResponse(code = 201, message="啥也不是，就想传一个码")
+    })
+    @PostMapping("/addNews")
+    public ResultMessage addNews(News news){
+
+        if(newsService.addNews(news)){
+            activeMQUtils.sendQueueMesage("addNews","请求添加新闻");
+
+            return ResultMessage.success();
+        }else{
+            return ResultMessage.error(CommonEnum.USER_NOT_NULL);
+        }
+
+    }
+
+    @ApiOperation(value = "这是修改新闻功能",notes = "修改新闻")
+    @ApiImplicitParam(name = "news",value = "news",dataType = "News")
+    @ApiResponses(value = {
+            @ApiResponse(code = 200,message = "修改成功"),
+            @ApiResponse(code = 500,message = "用户不能为空"),
+            @ApiResponse(code = 201, message="啥也不是，就想传一个码")
+    })
+    @PostMapping("/updateNews")
+    public ResultMessage updateNews(News news){
+
+
+        if(newsService.updateNews(news)){
+            activeMQUtils.sendQueueMesage("updateNews","请求修改新闻");
+
+            return ResultMessage.success();
+        }else{
+            return ResultMessage.error(CommonEnum.USER_NOT_NULL);
+        }
+
     }
 
 }
